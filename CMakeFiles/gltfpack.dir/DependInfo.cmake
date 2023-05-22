06838528fad7b292aa4807861e49297d265f675c
aramirez29 <alvaro@unionavatars.com>
2023-05-22 17:46:37 +0200

    Updated makefile

---
+++ CMakeFiles/gltfpack.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/animation.cpp" "CMakeFiles/gltfpack.dir/gltf/animation.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/animation.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/basistoktx.cpp" "CMakeFiles/gltfpack.dir/gltf/basistoktx.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/basistoktx.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/fileio.cpp" "CMakeFiles/gltfpack.dir/gltf/fileio.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/fileio.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/gltfpack.cpp" "CMakeFiles/gltfpack.dir/gltf/gltfpack.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/gltfpack.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/image.cpp" "CMakeFiles/gltfpack.dir/gltf/image.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/image.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/json.cpp" "CMakeFiles/gltfpack.dir/gltf/json.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/json.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/material.cpp" "CMakeFiles/gltfpack.dir/gltf/material.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/material.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/mesh.cpp" "CMakeFiles/gltfpack.dir/gltf/mesh.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/mesh.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/node.cpp" "CMakeFiles/gltfpack.dir/gltf/node.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/node.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/parsegltf.cpp" "CMakeFiles/gltfpack.dir/gltf/parsegltf.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/parsegltf.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/parseobj.cpp" "CMakeFiles/gltfpack.dir/gltf/parseobj.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/parseobj.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/stream.cpp" "CMakeFiles/gltfpack.dir/gltf/stream.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/stream.cpp.o.d"
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/gltf/write.cpp" "CMakeFiles/gltfpack.dir/gltf/write.cpp.o" "gcc" "CMakeFiles/gltfpack.dir/gltf/write.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Alvaro/Union/meshoptimizer/CMakeFiles/meshoptimizer.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
